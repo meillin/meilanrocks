126980637cf6cbdbf0bfcdeb68513685058de152
Meilan Lin <meilan.dk@gmail.com>
2015-06-07 14:51:05 +0200

    Add youtube video to gallery

--- data/flickr.php
+++ data/flickr.php
@@ -1,6 +1,6 @@
 <?php
-
-$url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=cc31489e474eea4a278adf54310b1fb7&photoset_id=".$_GET["galleryId"]."&user_id=132277818%40N04&extras=url_m&format=json&nojsoncallback=1&auth_token=72157652205400389-01d42a531ef28f51&api_sig=086d235906908b049e5e90e7cc005f60";
+$galleryId = $_GET['galleryId'];
+$url = "https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=d45c4b88ef7c4f4d94e1c4e69cb15f10&photoset_id=". $galleryId ."&user_id=132277818%40N04&extras=url_m&per_page=&format=json&nojsoncallback=1";
 $content=file_get_contents($url);
 echo $content;
 
--- js/gallery/gallery.js
+++ js/gallery/gallery.js
@@ -39,7 +39,7 @@
 				'screen and (max-width: 600px)': { columns: 1 },
 				'screen and (min-width: 600px) and (max-width: 800px)': { columns: 2 },
 				'screen and (min-width: 800px) and (max-width: 1000px)': { columns: 3 },
-				'screen and (min-width: 1000px)': { columns: 4 },
+				'screen and (min-width: 1000px)': { columns: 3 },
 			});
 		}
 
